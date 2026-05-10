adeadd61ca70a2c647d939b173064dd7df00d969
zhangwentan <379327004@qq.com>
2026-05-10 16:08:51 +0800

    添加sitemap生成插件

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    sitemap({
+      hostname: 'https://text-tools-c2f.pages.dev',
+    }),
   ],
+  server: {
+    open: true,
+    host: 'localhost',
+    port: 5173,
+  },
 })
